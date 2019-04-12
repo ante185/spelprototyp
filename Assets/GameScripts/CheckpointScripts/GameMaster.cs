d6043a97dac6f1ebf1bcd11ee5949ae9960e938b
TBEW <tristan.wikstrom@gmail.com>
2019-04-12 15:14:48 +0200

    Added CheckpointScripts

---
+++ Assets/GameScripts/CheckpointScripts/GameMaster.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+    FÖR ATT DETTA SCRIPT SKA FUNGERA BEHÖVS OCKSÅ Checkpoint.cs OCH PlayerPos.cs
+    Skapa ett empty object (Rekomenderat namn GameMaster). objektet behöver ingen specifik position eller storlek.
+    lägg till scriptet i objektet och sätt "Last Checkpoint Pos" till de exakta kordinaterna som spearen sak börja på.
+    Skapa en ny Tag för GameMaster och döp den till "GM". Detta är för senare!
+    Last Checkpoint Rot bör lämnas som den är.
+
+*/
+
+public class GameMaster : MonoBehaviour {
+
+    public Vector3 lastCheckpointPos;
+    public Quaternion lastCheckpointRot;
+
+}
