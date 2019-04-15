56e862bda2cabd624aaee48ffca0a3ddcfa8c270
emjp18 <49557865+emjp18@users.noreply.github.com>
2019-04-15 14:48:38 +0200

    projekt

---
+++ Assets/scripts/interfaces/IStateBase.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace Assets.Code.Interfaces
+{
+    public interface IStateBase
+    {
+        void StateUpdate();
+        void ShowIt();
+
+    }
+
+
+
+
+
+
+
+}
+
+
