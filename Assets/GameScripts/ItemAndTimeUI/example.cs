6a3c65a43993d1e8acdbd8d193d572c457d91c89
emjp18 <49557865+emjp18@users.noreply.github.com>
2019-04-15 12:32:52 +0200

    Update example.cs

--- Assets/GameScripts/ItemAndTimeUI/example.cs
+++ Assets/GameScripts/ItemAndTimeUI/example.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class example : MonoBehaviour {
     public Display pickUps;
+    private float delay = 5.0f;
+    private const int nrOf = 50;
+    private float[] itemTime = new float[nrOf];
+    GameObject[] item = new GameObject[nrOf];
+    private bool[] timeIncrease = new bool[nrOf];
+    private int counter = 0;
     // Use this for initialization
     void Start () {
 		
@@ -11,8 +17,35 @@ public class example : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+
+        for (int j = 0; j < counter; j++)
+        {
+
+            if (timeIncrease[j])
+            {
+                itemTime[j] += Time.deltaTime;
+
+                if (itemTime[j] > delay)
+                {
+
+
+                    item[j].SetActive(true);
+
+
+
+
+                    timeIncrease[j] = false;
+                    itemTime[j] = 0.0f;
+                }
+
+            }
+
+
+        }
+
+
+
+    }
 
 
 
@@ -20,9 +53,24 @@ public class example : MonoBehaviour {
     {
         if (other.gameObject.CompareTag("timeReduction"))
         {
+
+
+            if (item[counter] != other.gameObject)
+            {
+                item[counter] = other.gameObject;
+                timeIncrease[counter] = true;
+                counter += 1;
+
+
+
+
+            }
+
+
+
+
             other.gameObject.SetActive(false);
             pickUps.addTime();
-
         }
     }
 }
