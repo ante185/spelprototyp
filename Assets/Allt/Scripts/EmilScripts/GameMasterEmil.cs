187661600cc34cbb5082595344a66b3dc4839a32
emjp18 <49557865+emjp18@users.noreply.github.com>
2019-04-26 09:40:56 +0200

    fixat ljus

--- Assets/Allt/Scripts/EmilScripts/GameMasterEmil.cs
+++ Assets/Allt/Scripts/EmilScripts/GameMasterEmil.cs
@@ -15,125 +15,23 @@ using UnityEngine;
 
 public class GameMasterEmil : MonoBehaviour
 {
-    public string completeList;
-    private hss[] listScores = new hss[10];
-    private int nrOf = 0;
-    public Display clock;
-    private int time;
+   
+   
+   
     public Vector3 lastCheckpointPos;
     public Quaternion lastCheckpointRot;
-    private bool saveVar = false;
+   
 
    
 
   
 
-    private void Update()
-    {
-        if (clock.timeStop)
-        {
-            time = clock.timePassedAsInt;
-        }
-        
-    }
-
-    public void save()
-    {
-        hss highScore = new hss();
-        highScore.time = time;
-        highScore.name = " ";
-        if(nrOf > 9)
-        {
-            if (highScore.time < listScores[nrOf].time)
-            {
-                listScores[nrOf] = highScore;
-                nrOf += 1;
-            }
-        }
-       else if(nrOf < 10)
-        {
-            listScores[nrOf] = highScore;
-            nrOf += 1;
-        }
-
-        
-
-        for (int i = 0; i < nrOf - 1; i++)
-        {
-            int smallest = i;
-
-            for (int j = i + 1; j < nrOf; j++)
-            {
-                if (listScores[j].time < listScores[smallest].time)
-                {
-                    smallest = j;
-
-
-                }
+   
 
 
 
-            }
-
-            hss temp = listScores[smallest];
-            listScores[smallest] = listScores[i];
-            listScores[i] = temp;
-
-
-        }
-
-        for(int k=0; k<nrOf; k++)
-        {
-            completeList += listScores[k].name + " " + listScores[k].time.ToString() + "\n\n";
-
-
-
-
-        }
-
-        //BinaryFormatter bF = new BinaryFormatter();
-        FileStream fS = File.Create(Application.persistentDataPath + "HighScore.dat");
-
-        //bF.Serialize(fS, completeList);
-
-
-        //File.CreateText(Application.persistentDataPath + "HighScore.dat");
-
-        File.WriteAllText(Application.persistentDataPath + "HighScore.dat", completeList);
-
-        fS.Close();
-
-
-    }
-    public void loading()
-    {
-
-        if(File.Exists(Application.persistentDataPath + "HighScore.dat"))
-        {
-            //BinaryFormatter bf = new BinaryFormatter();
-            FileStream fS = File.Open(Application.persistentDataPath + "HighScore.dat", FileMode.Open);
-            //completeList = bf.Deserialize(fS);
-            completeList = fS.ToString();
-            fS.Close();
-        }
-
-
-    }
-
-    private void toSave()
-    {
-        saveVar = true;
-    }
-
+   
 
 }
 
-class hss
-{
 
-    public int time;
-    public string name;
-
-
-
-}
