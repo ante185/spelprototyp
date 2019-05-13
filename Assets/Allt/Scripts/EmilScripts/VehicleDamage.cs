5a019914c14422e8d2b8a9ac0da4c84fa0ff255e
emjp18 <49557865+emjp18@users.noreply.github.com>
2019-05-13 11:50:02 +0200

    kollision

--- Assets/Allt/Scripts/EmilScripts/VehicleDamage.cs
+++ Assets/Allt/Scripts/EmilScripts/VehicleDamage.cs
@@ -4,58 +4,30 @@ using UnityEngine;
 
 public class VehicleDamage : MonoBehaviour
 {
-    public int health = 20;
+    public int health = 100;
     public VehicleMovement2 vehicle;
     public allSound sound;
-    public float rayDistance = 0.5f;
 
-    private void FixedUpdate()
+
+    private void OnCollisionEnter(Collision other)
     {
-        takeDamage();
+        if(other.gameObject.tag == "hinder")
+        {
+            health = health - 10;
+            sound.source.PlayOneShot(sound.takingDamage);
+
+
+
+
+        }
+
+
+
+
     }
 
 
 
-    private void takeDamage()
-    {
-        Vector3 right = transform.TransformDirection(Vector3.right);
-        Vector3 left = transform.TransformDirection(Vector3.left);
-        Vector3 back = transform.TransformDirection(Vector3.back);
-        Vector3 forward = transform.TransformDirection(Vector3.forward);
-        RaycastHit hit;
-
-        if (Physics.Raycast(transform.position, right, out hit, rayDistance) && vehicle.getcurrentSpeed() >=5 && hit.transform.tag!="pickUp")
-        {
-            health -= 1;
-
-            sound.source.PlayOneShot(sound.takingDamage);
-
-        }
-
-
-
-
-        if (Physics.Raycast(transform.position, left, out hit, rayDistance) && vehicle.getcurrentSpeed() >= 5 && hit.transform.tag != "pickUp")
-        {
-            health -= 1;
-            sound.source.PlayOneShot(sound.takingDamage);
-        }
-        if (Physics.Raycast(transform.position, back, out hit, rayDistance) && vehicle.getcurrentSpeed() >= 5 && hit.transform.tag != "pickUp")
-        {
-
-            health -= 1;
-            sound.source.PlayOneShot(sound.takingDamage);
-        }
-
-         if (Physics.Raycast(transform.position, forward, out hit, rayDistance) && vehicle.getcurrentSpeed() >= 5 && hit.transform.tag != "pickUp")
-         {
-            health -= 1;
-            sound.source.PlayOneShot(sound.takingDamage);
-        }
-      
-
-
-    }
 
 
 }
