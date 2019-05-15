36d824d841010ce16528a5a3ddb5d032af5642ef
emjp18 <49557865+emjp18@users.noreply.github.com>
2019-05-15 13:05:35 +0200

    nästanfärdigt

--- Assets/Allt/Scripts/EmilScripts/VehicleDamage.cs
+++ Assets/Allt/Scripts/EmilScripts/VehicleDamage.cs
@@ -7,8 +7,15 @@ public class VehicleDamage : MonoBehaviour
     public int health = 100;
     public VehicleMovement2 vehicle;
     public allSound sound;
+    public switchTexture textSwitch;
+    private int baseHealth;
+    
+    private void Start()
+    {
+        baseHealth = health;
+    }
 
-
+   
     private void OnCollisionEnter(Collision other)
     {
         if(other.gameObject.tag == "hinder")
@@ -16,12 +23,20 @@ public class VehicleDamage : MonoBehaviour
             health = health - 10;
             sound.source.PlayOneShot(sound.takingDamage);
 
+            if(health<baseHealth/2)
+            {
+                textSwitch.takingDamage();
 
 
+            }
+            else
+            {
+                textSwitch.notTakingDamage();
+            }
 
         }
 
-
+        
 
 
     }
