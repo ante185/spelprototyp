a7cf056e81c999fe92046a8671563d1a48b775ea
emjp18 <49557865+emjp18@users.noreply.github.com>
2019-05-18 12:58:16 +0200

    medalj

---
+++ Assets/Allt/Scripts/EmilScripts/MenuSystemAndHighScore/gameOverMedals.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class gameOverMedals : MonoBehaviour {
+
+    public Sprite gold;
+    public Sprite silver;
+    public Sprite bronze;
+    public timeLoad time;
+
+    // Use this for initialization
+    void Start () {
+
+
+        if(time.timeToGive>180)
+        {
+            GetComponent<GameObject>().SetActive(false);
+
+        }
+        if (time.timeToGive <= 120)
+        {
+            GetComponent<Image>().sprite = gold;
+
+        }
+        if (time.timeToGive <= 150 && time.timeToGive >120)
+        {
+            GetComponent<Image>().sprite = silver;
+
+        }
+        if (time.timeToGive <= 180 && time.timeToGive > 150)
+        {
+            GetComponent<Image>().sprite = bronze;
+
+        }
+
+
+    }
+	
+	
+}
