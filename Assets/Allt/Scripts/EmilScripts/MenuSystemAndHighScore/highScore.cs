7152a36da2ff303ca21a073f98669e4751dbf08c
TBEW <tristan.wikstrom@gmail.com>
2019-05-20 15:41:14 +0200

    HighScoreList

--- Assets/Allt/Scripts/EmilScripts/MenuSystemAndHighScore/highScore.cs
+++ Assets/Allt/Scripts/EmilScripts/MenuSystemAndHighScore/highScore.cs
@@ -1,69 +1,69 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine.UI;
-using UnityEngine;
+﻿//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine.UI;
+//using UnityEngine;
 
-public class highScore : MonoBehaviour
-{
-    private string completeList;
-    private int latest;
-    public Text text;
-    private int lenght;
-	// Use this for initialization
-	void Start ()
-    {
-        latest = PlayerPrefs.GetInt("high");
+//public class highScore : MonoBehaviour
+//{
+//    private string completeList;
+//    private int latest;
+//    public Text text;
+//    private int lenght;
+//	// Use this for initialization
+//	void Start ()
+//    {
+//        latest = PlayerPrefs.GetInt("high");
 
-        completeList = PlayerPrefs.GetString("complete");
+//        completeList = PlayerPrefs.GetString("complete");
 
-        lenght= completeList.Split('\n').Length;
+//        lenght= completeList.Split('\n').Length;
         
 
 
-        if (completeList != null)
-        {
-            if (PlayerPrefs.GetInt("save") == 1)
-            {
-                if(lenght<12)
-                {
-                    completeList = completeList + "\n" + PlayerPrefs.GetString("name") + " " + latest.ToString();
-                    PlayerPrefs.SetString("complete", completeList);
+//        if (completeList != null)
+//        {
+//            if (PlayerPrefs.GetInt("save") == 1)
+//            {
+//                if(lenght<12)
+//                {
+//                    completeList = completeList + "\n" + PlayerPrefs.GetString("name") + " " + latest.ToString();
+//                    PlayerPrefs.SetString("complete", completeList);
 
-                }
+//                }
 
                 
 
 
 
-            }
+//            }
 
 
             
 
-        }
+//        }
 
 
 
-        if (completeList==null)
-        {
-            completeList = "High Score";
-            PlayerPrefs.SetString("complete", completeList);
-        }
+//        if (completeList==null)
+//        {
+//            completeList = "High Score";
+//            PlayerPrefs.SetString("complete", completeList);
+//        }
 
-        if (PlayerPrefs.GetInt("save") == 1)
-        {
-            PlayerPrefs.SetInt("save", 0);
+//        if (PlayerPrefs.GetInt("save") == 1)
+//        {
+//            PlayerPrefs.SetInt("save", 0);
 
 
 
-        }
+//        }
 
     
-        //PlayerPrefs.SetString("complete", "High Score");
-        text.text = PlayerPrefs.GetString("complete");
+//        //PlayerPrefs.SetString("complete", "High Score");
+//        text.text = PlayerPrefs.GetString("complete");
 
 
-	}
+//	}
 	
 	
-}
+//}
