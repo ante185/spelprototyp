b3f05e958e39cd71a2484cb7f8a36bc47eff3377
TBEW <tristan.wikstrom@gmail.com>
2019-05-20 13:31:12 +0200

    Update HighScoreTable.cs

--- Assets/Allt/Scripts/TristanScripts/HighScoreTable.cs
+++ Assets/Allt/Scripts/TristanScripts/HighScoreTable.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class HighScoreEntryContainer : MonoBehaviour
+public class HighScoreTable : MonoBehaviour
 {
     private Transform entryContainer;
     private Transform entryTemplate;
