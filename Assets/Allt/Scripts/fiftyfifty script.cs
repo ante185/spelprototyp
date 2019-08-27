d3f22b0c3c274dd24cd2f98c89a933bc94606626
Ante <49555217+ante185@users.noreply.github.com>
2019-08-27 18:58:02 +0200

    Started script

---
+++ Assets/Allt/Scripts/fiftyfifty script.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fiftyfiftyscript : MonoBehaviour {
+
+    private Renderer mesh;
+    private Collider collider;
+
+    // Use this for initialization
+    void Start ()
+    {
+        mesh = GetComponent<MeshRenderer>();
+        collider = GetComponent<BoxCollider>();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    void OnTriggerEnter(Collider other)
+    {
+
+
+        if (other.gameObject.CompareTag("Player"))
+        {
+            mesh.enabled = false;
+            collider.enabled = false;
+        }
+
+
+
+    }
+}
